c570340346372e4dceb0dd66129e9cbd3da7b409
Phạm Trần Thuỷ Vy <vy.ptt.64cntt@ntu.edu.vn>
2025-02-12 18:27:56 +0700

    Update XinChao.java
    Sửa đổi và thêm dòng mới

--- HelloWorld/src/XinChao.java
+++ HelloWorld/src/XinChao.java
@@ -1,6 +1,8 @@
 public class XinChao {
 
     public static void main(String[] args) {
-        System.out.println("Xin chào các bạn! Tôi lần đầu học Java và đang thử nghiệm mã nguồn với Git :3 ");
+        System.out.println("Xin chào các bạn! Tôi lần đầu học Java và đang thử nghiệm mã nguồn với Git");
+        System.out.println("Chúc các bạn học tốt!");
+        System.out.println("Tôi đã thêm 2 dòng và xoá :3 ở dòng đầu tiên");
     }
 }
