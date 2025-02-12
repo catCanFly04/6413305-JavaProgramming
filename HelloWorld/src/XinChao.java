47bc0113ddca0e4391c6b3a1afc446fe9b0a291b
Phạm Trần Thuỷ Vy <vy.ptt.64cntt@ntu.edu.vn>
2025-02-12 18:19:16 +0700

    Ví dụ đầu tiên
    Chỉ Demo

---
+++ HelloWorld/src/XinChao.java
@@ -0,0 +1,6 @@
+public class XinChao {
+
+    public static void main(String[] args) {
+        System.out.println("Xin chào các bạn! Tôi lần đầu học Java và đang thử nghiệm mã nguồn với Git :3 ");
+    }
+}
