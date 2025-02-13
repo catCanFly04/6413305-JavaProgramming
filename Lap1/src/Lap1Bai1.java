62df1ad50ccba8b01a1dc5745a52ab5983bfaca6
Phạm Trần Thuỷ Vy <vy.ptt.64cntt@ntu.edu.vn>
2025-02-13 20:57:14 +0700

    bài 1 lap 1

---
+++ Lap1/src/Lap1Bai1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Lap1Bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập họ và tên: ");
+        String hoTen = scanner.nextLine();
+        System.out.print("Nhập điểm trung bình: ");
+        double diemTB = scanner.nextDouble();
+        System.out.printf("%s %f điểm",  hoTen ,  diemTB);
+
+    }
+}
