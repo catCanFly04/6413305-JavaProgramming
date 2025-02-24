c979b6c2f211fc071009a475374c444b4f97a2a5
Phạm Trần Thuỷ Vy <vy.ptt.64cntt@ntu.edu.vn>
2025-02-24 21:06:27 +0700

    Bảng cửu chương

---
+++ Lab3/src/Lab3Bai2.java
@@ -0,0 +1,10 @@
+public class Lab3Bai2 {
+    public static void main(String[] args) {
+         for (int x = 1; x < 10; x++) { 
+            for (int i = 1; i <= 10; i++) { 
+                System.out.println(x + " x " + i + " = " + (x * i));
+            }
+            System.out.println(); 
+        }
+    }
+}
